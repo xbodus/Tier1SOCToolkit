9851804e1c577017e0289e1dfd9aa00884b82bdb
Xavier Bodus <xbodus@gmail.com>
2025-07-30 17:10:17 -0400

    env update

--- SOCTeir1Toolkit_CapstoneProject/settings.py
+++ SOCTeir1Toolkit_CapstoneProject/settings.py
@@ -11,16 +11,21 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
 from pathlib import Path
+import os
+import environ
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
+env = environ.Env(
+    DEBUG=(bool, False)
+)
+environ.Env.read_env(env_file = os.path.join(BASE_DIR, ".env"))
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-%id$s#!ogv#@cifz9ci=9c=0=y0wzquhz8fonewv^*549#+69v'
+SECRET_KEY = env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
