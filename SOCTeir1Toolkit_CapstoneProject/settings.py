d473bdc5faf8fd67342b28452da1e41b72a5fd6b
Xavier Bodus <xbodus@gmail.com>
2025-07-30 17:29:25 -0400

    env update

--- SOCTeir1Toolkit_CapstoneProject/settings.py
+++ SOCTeir1Toolkit_CapstoneProject/settings.py
@@ -28,7 +28,7 @@ environ.Env.read_env(env_file = os.path.join(BASE_DIR, ".env"))
 SECRET_KEY = env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = env("DEBUG")
 
 ALLOWED_HOSTS = []
 
@@ -82,7 +82,7 @@ WSGI_APPLICATION = 'SOCTeir1Toolkit_CapstoneProject.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': BASE_DIR / env('DB_NAME'),
     }
 }
 
