b1a94106f1eaf0c9a99a5d097cc6e5f0523d6e49
Xavier Bodus <xbodus@gmail.com>
2025-11-03 19:26:52 -0500

    Added React using Django-Vite. Began dashboard layout

--- frontend/src/Dashboard/components/Application.tsx
+++ frontend/src/Dashboard/components/Application.tsx
@@ -1,16 +1,24 @@
-
+import AppWindow from "../Utils/Window.tsx";
+import { useState } from "react";
 
 type Props = {
     label: string
 }
 
 export default function Application(props: Props) {
+    const [mount, setMount] = useState(false)
+
     return (
-        <div className='application-container'>
+        <div className='application-container' onClick={() => setMount(true)}>
             <div>
                 <div></div>
             </div>
             <p>{props.label}</p>
+            {mount && (
+                <AppWindow>
+                    <p>New window</p>
+                </AppWindow>
+            )}
         </div>
     )
 }
